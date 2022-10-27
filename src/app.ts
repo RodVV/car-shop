bf21e47163cb2264282e0452f95fe9344df430bb
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-10-26 21:44:51 -0300

    rota post Car

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import carRoute from './routes/car';
 
 const app = express();
+app.use(express.json());
+app.use(carRoute);
+app.use(errorHandler);
 
 export default app;
---
+++ src/routes/car.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/Car';
+import Car from '../models/Car';
+import CarService from '../services/Car';
+
+const carRoute = Router();
+
+const car = new Car();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+carRoute.post('/car', (req, res) => carController.create(req, res));
+
+export default carRoute;
