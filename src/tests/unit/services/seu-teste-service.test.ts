aa7f3d10a50105be97dd217d47c7f284fcf405d2
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-10-30 20:55:41 -0300

    add tests camada service para read e readOne, teste de falha bugando

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import Car from '../../../models/Car'
 import { carMock, carMockWithId, carMockArrayWithId } from '../../mocks/carMock';
 import CarService from '../../../services/Car';
+import { ErrorTypes } from '../../../middlewares/errorTypes';
 
 describe('Car Services', () => {
   const carModel = new Car();
@@ -18,6 +19,32 @@ describe('Car Services', () => {
       sinon.stub(carModel, 'create').resolves(carMockWithId);
 			const newCar = await carService.create(carMock);
 			expect(newCar).to.be.deep.equal(carMockWithId);
+		});
+	});
+
+  describe('read all cars', () => {
+		it('successfully read', async () => {
+      sinon.stub(carModel, 'read').resolves(carMockArrayWithId);
+			const getCars = await carService.read();
+			expect(getCars).to.be.deep.equal(carMockArrayWithId);
+		});
+	});
+
+  describe('read car by id', () => {
+		it('successfully read', async () => {
+      sinon.stub(carModel, 'readOne').resolves(carMockWithId);
+			const getCar = await carService.readOne(carMockWithId._id);
+			expect(getCar).to.be.deep.equal(carMockWithId);
+		});
+    it('failed to read: not found', async () => {
+      let error;
+      sinon.stub(carModel, 'readOne').resolves(null);
+      try {
+        await carService.readOne(carMockWithId._id);        
+      } catch (err: any) {
+        error = err;
+      }
+      expect(error?.error).to.be.deep.equal(ErrorTypes.EntityNotFound);        
 		});
 	});
 });
