45db3df35aeab867a730086b880c25869a302452
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-10-30 22:07:11 -0300

    conserta o nome do tests de controller de Car Services para Car Controllers, add testes para camada controlller das funcoes read e readOne da rota /cars

--- src/tests/unit/controllers/seu-test-controller.test.ts
+++ src/tests/unit/controllers/seu-test-controller.test.ts
@@ -6,14 +6,14 @@ import { carMock, carMockWithId, carMockArrayWithId } from '../../mocks/carMock'
 import CarService from '../../../services/Car';
 import CarController from '../../../controllers/Car';
 
-describe('Car Services', () => {
+describe('Car Controllers', () => {
   const carModel = new Car();
   const carService = new CarService(carModel);
   const carController = new CarController(carService);
   const req = {} as Request; 
   const res = {} as Response;
 
-  before(() => {     
+  beforeEach(() => {     
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
   });
@@ -35,4 +35,25 @@ describe('Car Services', () => {
       expect(jsonStub.calledWith(carMock)).to.be.true;
 		});
 	});
+
+  describe('Read all cars', () => {
+    it('Success', async () => {
+      sinon.stub(carService, 'read').resolves([carMockWithId]);
+      await carController.read(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith([carMockWithId])).to.be.true;
+    });
+  });
+
+  describe('Read one car', () => {
+    it('Success', async () => {
+      sinon.stub(carService, 'readOne').resolves(carMockWithId);
+      req.params = { id: carMockWithId._id };
+      await carController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMockWithId)).to.be.true;
+    });
+  });
 });
